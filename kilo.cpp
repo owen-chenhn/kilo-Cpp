0f3c71a2114c2edf76d9683686457f38a76dc5fd
Haoning Chen <owenchen97@cs.uchicago.edu>
2020-09-13 18:55:58 -0500

    Read in one char at a time until reaching 'q'.

--- kilo.cpp
+++ kilo.cpp
@@ -1,7 +1,17 @@
 /* The main file of kilo in C++ implementation. */
 #include <iostream>
+#include <cstdio>
+
+using namespace std;
 
 int main() {
-    std::cout << "Test C++ compilation." << std::endl;
+    std::cout << "Text editor Kilo - C++ version.\n";
+
+    // Read in char one by one until see 'q'
+    char c;
+    while ( (c = getchar()) != EOF && c != 'q' ) {
+        cout << c << endl;
+    };
+    
     return 0;
 }
