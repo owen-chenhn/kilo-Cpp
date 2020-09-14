8b79f40250aef98bf5e2db3b08345e499b59da78
Haoning Chen <owenchen97@cs.uchicago.edu>
2020-09-13 20:00:49 -0500

    Disable ECHO mode of terminal.

--- kilo.cpp
+++ kilo.cpp
@@ -1,11 +1,33 @@
 /* The main file of kilo in C++ implementation. */
 #include <iostream>
 #include <cstdio>
+#include <cstdlib>
+#include <unistd.h>
+#include <termios.h>
 
 using namespace std;
 
+
+static struct termios orig_termios;
+
+void disableRawMode() {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+/* Turn on Raw Mode of terminal. */
+void enableRawMode() {
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disableRawMode);
+
+    struct termios raw = orig_termios;
+    raw.c_lflag &= ~(ECHO);
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+
 int main() {
     std::cout << "Text editor Kilo - C++ version.\n";
+    enableRawMode();
 
     // Read in char one by one until see 'q'
     char c;
