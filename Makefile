388a92d0889f9898388d2a8199563e54f6c7073d
Haoning Chen <owenchen97@cs.uchicago.edu>
2020-10-07 22:47:29 -0500

    Make the code Object-Oriented.

--- Makefile
+++ Makefile
@@ -1,7 +1,16 @@
 # The make file for the kilo program
-
-# gcc for C program
 CC = g++
+CFLAG = -Wall -Wextra -pedantic -std=c++11
 
-kilo: kilo.cpp
-	$(CC) kilo.cpp -o kilo -Wall -Wextra -pedantic -std=c++11
+kilo: kilo.o main.o
+	$(CC) main.o kilo.o -o kilo $(CFLAG)
+
+main.o: main.cpp
+	$(CC) -c main.cpp -o main.o $(CFLAG)
+
+kilo.o: kilo.cpp
+	$(CC) -c kilo.cpp -o kilo.o $(CFLAG)
+
+
+clean:
+	rm *.o kilo
--- kilo.cpp
+++ kilo.cpp
@@ -1,67 +1,26 @@
-/* The main file of kilo in C++ implementation. */
-#include <iostream>
-#include <cctype>
-#include <cstdio>
-#include <cstdlib>
-#include <unistd.h>
-#include <termios.h>
-#include <sys/ioctl.h>
-
-
-#define CTRL_KEY(c) ((c) & 0x1f)
+/* The implementation file of kilo-cpp. */
+#include "kilo.h"
 
 /***  Global data  ***/
-struct editorConfig {
-    struct termios orig_termios;
-    int screenRows;
-    int screenCols;
-
-    int setWindowSize() {
-        struct winsize ws;
-        if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) 
-            return -1;
-        screenRows = ws.ws_row;
-        screenCols = ws.ws_col;
-        return 0;
-    }
-};
-const char *CLEAR_SCREEN = "\x1b[2J";
-const char *REPOS_CURSOR = "\x1b[H";
+const char *Kilo::CLEAR_SCREEN = "\x1b[2J";
+const char *Kilo::REPOS_CURSOR = "\x1b[H";
 
 
 /***  Terminal setup  ***/
-static editorConfig config;
-
-// Clear the screen and reposition the cursor.
-void clearScreen() {
-    std::cout << CLEAR_SCREEN;
-}
-void reposCursor() {
-    std::cout << REPOS_CURSOR;
-}
-
-
-/* Error handling function. */
-void die(const char *str) {
-    clearScreen();
-    reposCursor();
-
-    perror(str);
-    exit(1);
+int Kilo::setWindowSize() {
+    struct winsize ws;
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) 
+        return -1;
+    screenRows = ws.ws_row;
+    screenCols = ws.ws_col;
+    return 0;
 }
 
-void disableRawMode() {
-    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &config.orig_termios) == -1)
-        die("tcsetattr failed");
-}
-
-/* Turn on Raw Mode of terminal. */
-void enableRawMode() {
-    if (tcgetattr(STDIN_FILENO, &config.orig_termios) == -1) 
+void Kilo::enableRawMode() {
+    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) 
         die("tcgetattr failed");
-    atexit(disableRawMode);
 
-    termios raw = config.orig_termios;
+    termios raw = orig_termios;
     raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
     raw.c_oflag &= ~(OPOST);
     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
@@ -75,67 +34,48 @@ void enableRawMode() {
 
 
 /***  Input Handling  ***/
-char readKey() {
+char Kilo::readKey() {
     char c;
     // Read input every 0.1 sec. Return -1 (EOF) to c if no char is read. 
     while ( (c = getchar()) == EOF ) {}
     return c;
 }
 
-void editorProcessKeypress() {
+bool Kilo::processKeypress() {
     char c = readKey();
+    bool flag = true;
 
     switch (c) {
         case CTRL_KEY('q'):    // Ctrl-Q
             clearScreen();
             reposCursor();
-            exit(0);
+            flag = false;
             break;
 
         default:
             //TODO
             if (iscntrl(c)) {
-                std::cout << (int) c << "\r\n";
+                std::cout << (int) c << " ";
             }
             else {
-                std::cout << c << "\r\n";
+                std::cout << c;
             }
     }
+    return flag;
 }
 
 
 /***  Output Handling  ***/
-void editorDrawRows() {
+void Kilo::drawRows() {
     // Draw tilds at the beginning of each row.
-    for (int r = 0; r < config.screenRows - 1; r++) {
+    for (int r = 0; r < screenRows - 1; r++) {
         std::cout << "~\r\n";
     }
     std::cout << "~";
 }
 
-void editorRefreshScreen() {
-    clearScreen();
-    reposCursor();
 
-    editorDrawRows();
-    reposCursor();
-}
-
-
-/***  Init  ***/
-void initEditor() {
-    if (config.setWindowSize() == -1) die("setWindowSize");
-}
-
-int main() {
-    enableRawMode();
-    initEditor();
-    editorRefreshScreen();
-
-    std::cout << "Text editor Kilo - C++ version.\r\n";
-    while (true) {
-        editorProcessKeypress();
-    }
-    
-    return 0;
+/***  Public interface  ***/
+void Kilo::run() {
+    while ( processKeypress() ) ;
 }
